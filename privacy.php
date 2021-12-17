a3616501b2e34e18cbb449d3423fdd3bb803c700
Hamza Khan <hamza.khan@technado.pk>
2021-12-17 22:39:53 +0500

    dodgepoop

---
+++ privacy.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php';?>
+<div class="privacy-sec-2">
+<section class="index-sec-1">
+   <div class="header-textwrap">
+      <div class="header-textwrap">
+          <p><span class="download-text">Privacy<br></span><span class="play-wrap"><span class="play-text">and</span><span class="fun-text">Policy</span></span></p>
+
+      </div>
+   </div>
+   <img src="assets/images/bird1.png" class="img-fluid bird-1 ">
+   <img src="assets/images/bird-2.png" class="img-fluid bird-2 ">
+   <img src="assets/images/bird-3.png" class="img-fluid bird-3">
+</section>
+    
+</div>
+<section class="privacy-sec-1">
+   <div class="container">
+      <div class="sec-2wrap">
+         <div class="sec-2content mt-4">
+            <p class="sec2-text1 headings">Privacy</p>
+             <p class="privacy-text1">This Privacy Policy applies to our website, and its associated subdomains (collectively, our “Service”) alongside our application, Dodge Poop. By accessing or using our Service, you signify that you have read, understood, and agree to our collection, storage, use, and disclosure of your personal information as described in this Privacy Policy and our Terms of Service.</p>
+         </div>
+      </div>
+   </div>
+</section>
+<?php include 'includes/footer.php';?>
---
+++ terms.php
@@ -0,0 +1,25 @@
+<?php include 'includes/header.php';?>
+<div class="privacy-sec-2">
+<section class="index-sec-1">
+   <div class="header-textwrap">
+      <div class="header-textwrap">
+          <p><span class="download-text">Terms<br></span><span class="play-wrap"><span class="play-text">and</span><span class="fun-text">Condition</span></span></p>
+      </div>
+   </div>
+   <img src="assets/images/bird1.png" class="img-fluid bird-1 ">
+   <img src="assets/images/bird-2.png" class="img-fluid bird-2 ">
+   <img src="assets/images/bird-3.png" class="img-fluid bird-3">
+</section>
+</div>
+<section class="privacy-sec-1">
+   <div class="container">
+      <div class="sec-2wrap">
+         <div class="sec-2content mt-4">
+            <p class="sec2-text1 headings">General Terms</p>
+             <p class="privacy-text1">By accessing and placing an order with Dodge Poop, you confirm that you are in agreement with and bound by the terms ofservice contained in the Terms & Conditions outlined below. These terms apply to the entire website and any email or other typeof communication between you and Dodge Poop.
+</p>
+         </div>
+      </div>
+   </div>
+</section>
+<?php include 'includes/footer.php';?>
